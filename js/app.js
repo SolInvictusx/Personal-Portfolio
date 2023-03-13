3404a6b23211683f0fcc83f1e6a0f4e3984c4867
SolInvictusx <ambrosi52@live.com>
2023-03-12 20:36:32 -0500

    Current

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
 //variables
 
-const cartBtn = document.querySelector('.cart-btn');
-const closeCartBtn = document.querySelector('.close-cart');
-const clearCartBtn = document.querySelector('.clear-cart');
-const cartDOM = document.querySelector('.cart');
-const cartOverlay = document.querySelector('.cart-overlay');
-const cartItems = document.querySelector('.cart-items');
-const cartTotal = document.querySelector('.cart-total');
-const cartContent = document.querySelector('.cart-content');
-const productsDOM = document.querySelector('.products-center');
+const cartBtn = document.querySelector(".cart-btn");
+const closeCartBtn = document.querySelector(".close-cart");
+const clearCartBtn = document.querySelector(".clear-cart");
+const cartDOM = document.querySelector(".cart");
+const cartOverlay = document.querySelector(".cart-overlay");
+const cartItems = document.querySelector(".cart-items");
+const cartTotal = document.querySelector(".cart-total");
+const cartContent = document.querySelector(".cart-content");
+const productsDOM = document.querySelector(".products-center");
 // Cart 
 let cart = [];
 
@@ -18,7 +18,14 @@ class Products {
         try {
             let result = await fetch("products.json")
             let data = await result.json();
-            return data;
+            let products = data.items;
+            products = products.map(item => {
+                const { title, price } = item.fields;
+                const { id } = item.sys
+                const image = item.fields.image.fields.file.url;
+                return { title, price, id, image }
+            })
+            return products
         } catch (error) {
             console.log(error);
         }
